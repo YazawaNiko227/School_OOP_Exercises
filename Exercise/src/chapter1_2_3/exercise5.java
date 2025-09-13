69bdf92c646e975fe33a7bd653c8b647eab76110
nicol <nicol@YazawaNiko>
2025-09-13 22:47:57 +0700

    Finished exercise 5

---
+++ Exercise/src/chapter1_2_3/exercise5.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package chapter1_2_3;
+
+import java.util.Scanner;
+
+/**
+ * @description Iuh_Fit_Cs_Stt36_24741631
+ * @author Phan Đức Toàn
+ * @version 1.0
+ * @created date13 thg 9, 2025 time22:44:04
+ */
+public class exercise5 {
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		String myName = input.nextLine();
+		System.out.printf("Hello " + myName );
+	}
+}
