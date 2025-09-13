6957dc8e3701b271d3f1dddac4c483587383c07c
nicol <nicol@YazawaNiko>
2025-09-13 23:02:28 +0700

    finished exercise 8

---
+++ Exercise/src/chapter1_2_3/exercise8.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package chapter1_2_3;
+
+/**
+ * @description Iuh_Fit_Cs_Stt36_24741631
+ * @author Phan Đức Toàn
+ * @version 1.0
+ * @created date13 thg 9, 2025 time23:00:52
+ */
+public class exercise8 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int value1 = 1;
+		int value2 = 2;
+		int result;
+		boolean someCondition = true;
+		result = someCondition ? value1 : value2;
+		System.out.println(result);
+	}
+
+}
