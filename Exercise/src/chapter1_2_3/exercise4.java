b39855aaf1538e5120eb8c3a6fa20939f41950bc
nicol <nicol@YazawaNiko>
2025-09-13 22:41:54 +0700

    Finished exercise 4

---
+++ Exercise/src/chapter1_2_3/exercise4.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package chapter1_2_3;
+
+/**
+ * @description Iuh_Fit_Cs_Stt36_24741631
+ * @author Phan Đức Toàn
+ * @version 1.0
+ * @created date13 thg 9, 2025 time22:40:50
+ */
+public class exercise4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
