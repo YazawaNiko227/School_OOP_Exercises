90bc98b5f6ca946ec1e6de39705d8fdf2103bfa9
nicol <nicol@YazawaNiko>
2025-09-13 23:32:48 +0700

    Finished exercise 11

--- Exercise/src/chapter1_2_3/exercise11.java
+++ Exercise/src/chapter1_2_3/exercise11.java
@@ -7,19 +7,19 @@ package chapter1_2_3;
  * @description Iuh_Fit_Cs_Stt36_24741631
  * @author Phan Đức Toàn
  * @version 1.0
- * @created date13 thg 9, 2025 time23:20:57
+ * @created date13 thg 9, 2025 time23:26:18
  */
-public class exercise10 {
+public class exercise11 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int sum = 0;
-		int count = 0;
-		while(count++ < 10) {
-			sum += count;
+		int number = 0;
+		while(number++ < 100) {
+			if(number % 2 != 0) {
+				System.out.println(number);
+			}
 		}
-		System.out.println(sum);
 	}
 }
