db78e15c397dead4b96f21cae47f3031d6f11d1b
nicol <nicol@YazawaNiko>
2025-09-13 23:24:19 +0700

    Finished exercise 10

---
+++ Exercise/src/chapter1_2_3/exercise10.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package chapter1_2_3;
+
+/**
+ * @description Iuh_Fit_Cs_Stt36_24741631
+ * @author Phan Đức Toàn
+ * @version 1.0
+ * @created date13 thg 9, 2025 time23:20:57
+ */
+public class exercise10 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int sum = 0;
+		int count = 1;
+		while(count++ <= 10) {
+			sum += count;
+		}
+		System.out.println(sum);
+	}
+}
