89b7beac5b71bfa6e7e1dd221e121da1c84f6f54
nicol <nicol@YazawaNiko>
2025-09-16 15:16:59 +0700

    Finished exercise 20

---
+++ Exercise/src/chapter1_2_3/exercise20.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package chapter1_2_3;
+
+import java.util.Scanner;
+
+/**
+ * @description Iuh_Fit_Cs_Stt36_24741631
+ * @author Phan Đức Toàn
+ * @version 1.0
+ * @created date16 thg 9, 2025 time15:13:56
+ */
+public class exercise20 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		String string = input.nextLine();
+		int count = 0;
+		for (int i = 0; i < string.length(); i++) {
+			if(Character.isDigit(string.charAt(i))) {
+				count++;
+			}
+		}System.out.println(count);
+	}
+
+}
