e00583e70ff957142f0a909b58e8bfe9cee16ed2
nicol <nicol@YazawaNiko>
2025-09-16 15:58:30 +0700

    Finished exercise 27

--- Exercise/src/chapter1_2_3/Exercise27.java
+++ Exercise/src/chapter1_2_3/Exercise27.java
@@ -3,6 +3,8 @@
  */
 package chapter1_2_3;
 
+import java.util.Scanner;
+
 /**
  * @description Iuh_Fit_Cs_Stt36_24741631
  * @author Phan Đức Toàn
@@ -15,8 +17,23 @@ public class Exercise27 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		  try (Scanner scanner = new Scanner(System.in)) {
+			System.out.print("Nhập số nguyên n: ");
+	        int n = scanner.nextInt();
 
+	        int a = 1, b = 1;
+
+	        System.out.print("Chuỗi Fibonacci: ");
+	        if (n >= 1) System.out.print(a + " ");
+	        if (n >= 2) System.out.print(b + " ");
+
+	        for (int i = 3; i <= n; i++) {
+	            int next = a + b;
+	            System.out.print(next + " ");
+	            a = b;
+	            b = next;
+	        }
+		  }
 	}
 
 }
