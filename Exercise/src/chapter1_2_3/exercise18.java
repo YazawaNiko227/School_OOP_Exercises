de8c0b2d34f3529c025bd1113ffcf0158940c821
nicol <nicol@YazawaNiko>
2025-09-16 15:04:59 +0700

    Finished exercise 18

---
+++ Exercise/src/chapter1_2_3/exercise18.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package chapter1_2_3;
+
+/**
+ * @description Iuh_Fit_Cs_Stt36_24741631
+ * @author Phan Đức Toàn
+ * @version 1.0
+ * @created date16 thg 9, 2025 time14:59:51
+ */
+public class exercise18 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int basicNumber = 3;
+		System.out.println("Boi cua 3 la: ");
+		for (int i = 3; i <= 300; i += 3) {
+			if(i % 3 == 0) {
+				System.out.printf(i + " ");
+			}
+		}
+	}
+
+}
