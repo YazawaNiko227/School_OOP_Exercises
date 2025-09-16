5882a0efecca9db6ca56a73bf57c891a4e9fd2e3
nicol <nicol@YazawaNiko>
2025-09-16 15:51:39 +0700

    Finished exercise 25

---
+++ Exercise/src/chapter1_2_3/exercise25.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package chapter1_2_3;
+
+import java.util.Scanner;
+
+/**
+ * @description Iuh_Fit_Cs_Stt36_24741631
+ * @author Phan Đức Toàn
+ * @version 1.0
+ * @created date16 thg 9, 2025 time15:44:33
+ */
+public class exercise25 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		int num = input.nextInt();
+		int sum = 0;
+		for (int i = 2; i < num; i++) {
+			if(checkPrime(i)) {
+				sum += i;
+			}
+		}System.out.println(sum);
+	}
+
+	/**
+	 * @param i
+	 * @return
+	 */
+	private static boolean checkPrime(int number) {
+		for (int i = 2; i <= Math.sqrt(number); i++) {
+			if(number % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
