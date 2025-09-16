40825abf9d9a1df9b32f55dce4457f34c8c5749f
nicol <nicol@YazawaNiko>
2025-09-16 15:44:14 +0700

    Finished exercise 24

---
+++ Exercise/src/chapter1_2_3/exercise24.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package chapter1_2_3;
+
+import java.util.Scanner;
+
+
+/**
+ * @description Iuh_Fit_Cs_Stt36_24741631
+ * @author Phan Đức Toàn
+ * @version 1.0
+ * @created date16 thg 9, 2025 time15:39:26
+ */
+public class exercise24 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		int inputNumber = input.nextInt();
+		int sum = 0;
+		for (int i = 0; i < inputNumber; i++) {
+			int number = input.nextInt();
+			sum += number;
+		}
+		System.out.println(sum);
+	}
+}
