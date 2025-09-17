9a4cb85fa35e65edb61c345aa18013e6da672907
nicol <nicol@YazawaNiko>
2025-09-17 17:30:55 +0700

    Finished create getter and setter

---
+++ Exercise/src/bai03/Vehicle.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package bai03;
+
+/**
+ * @description Iuh_Fit_Cs_Stt36_24741631
+ * @author Phan Đức Toàn
+ * @version 1.0
+ * @created date17 thg 9, 2025 time17:28:22
+ */
+public class Vehicle {
+	private String ownerName;
+	private String typeVehicle;
+	private int xylanh;
+	private double price;
+	/**
+	 * @return the ownerName
+	 */
+	public String getOwnerName() {
+		return ownerName;
+	}
+	/**
+	 * @param ownerName the ownerName to set
+	 */
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+	/**
+	 * @return the typeVehicle
+	 */
+	public String getTypeVehicle() {
+		return typeVehicle;
+	}
+	/**
+	 * @param typeVehicle the typeVehicle to set
+	 */
+	public void setTypeVehicle(String typeVehicle) {
+		this.typeVehicle = typeVehicle;
+	}
+	/**
+	 * @return the xylanh
+	 */
+	public int getXylanh() {
+		return xylanh;
+	}
+	/**
+	 * @param xylanh the xylanh to set
+	 */
+	public void setXylanh(int xylanh) {
+		this.xylanh = xylanh;
+	}
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+}
