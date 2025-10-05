f2c38369ddcbd36d0a595f7bf8c384bc1c39b3ea
Phan Đức Toàn <phanductoan2006@gmail.com>
2025-10-05 14:37:40 +0700

    Class Student

---
+++ Exercise/src/chapter4_exercise04/Student.java
@@ -0,0 +1,36 @@
+package chapter4_exercise04;
+
+import java.time.LocalDate;
+
+public class Student extends Person{
+	private String studentID;
+	private int yearMatriculated;
+	public Student(String lastName, String firstName, String studentID, int yearMatriculated) {
+		super(lastName, firstName);
+		setStudentID(studentID);
+		setYearMatriculated(yearMatriculated);
+	}
+	public Student(String lastName, String firstName) {
+		super(lastName, firstName);
+		setStudentID("Empty");
+		setYearMatriculated(LocalDate.now().getYear());
+	}
+	public String getStudentID() {
+		return studentID;
+	}
+	public void setStudentID(String studentID) {
+		if(studentID == null || studentID.isEmpty()) {
+			studentID = "Empty";
+		}
+		this.studentID = studentID;
+	}
+	public int getYearMatriculated() {
+		return yearMatriculated;
+	}
+	public void setYearMatriculated(int yearMatriculated) {
+		if(yearMatriculated > LocalDate.now().getYear()) {
+			yearMatriculated = LocalDate.now().getYear();
+		}
+		this.yearMatriculated = yearMatriculated;
+	}
+}
