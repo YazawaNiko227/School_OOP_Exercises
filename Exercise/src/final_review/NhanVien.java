79b65eb48a765182526584a1e692ef0f5eff8563
Phan Đức Toàn <phanductoan2006@gmail.com>
2025-10-22 12:01:54 +0700

    Finished class QuanLy

--- Exercise/src/final_review/NhanVien.java
+++ Exercise/src/final_review/NhanVien.java
@@ -1,6 +1,7 @@
 package final_review;
 
 import java.text.DecimalFormat;
+import java.util.Objects;
 
 public abstract class NhanVien {
 	protected final String maSo;
@@ -55,7 +56,24 @@ public abstract class NhanVien {
 	}
 	
 	protected abstract double tienLuong();
-	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(maSo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+		NhanVien other = (NhanVien) obj;
+		return Objects.equals(maSo, other.maSo);
+	}
+
 	@Override
 	public String toString() {
 		return String.format("%10s | %15s | %15s | %10s | ", 
---
+++ Exercise/src/final_review/QuanLyNhanVien.java
@@ -0,0 +1,59 @@
+package final_review;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+public class QuanLyNhanVien {
+	private List<NhanVien> quanLy;
+	
+	public QuanLyNhanVien() {
+		quanLy = new ArrayList<NhanVien>();
+	}
+	
+	public boolean themNhanVien(NhanVien nhanVienMoi) {
+		if(nhanVienMoi == null || quanLy.contains(nhanVienMoi)) {
+			return false;
+		}
+		quanLy.add(nhanVienMoi);
+		return true;
+	}
+	
+	public Map<String, Double> thongKeLuongTrungBinh(){
+	    return quanLy.stream().collect(
+	    		Collectors.groupingBy(nv -> nv.getClass().getSimpleName()
+	    				, Collectors.averagingDouble(nv -> nv.tienLuong())
+	    		));
+	}
+	public List<NhanVien> getDSNhanVienUuTu(){
+		return quanLy.stream()
+			.filter(nv -> nv instanceof NhanVienBanHang nvbh
+					&& nvbh.getDoanhSo() < 2e7)
+			.sorted(Comparator.comparingDouble(nv -> ((NhanVienBanHang)nv)
+					.getDoanhSo()).reversed())
+			.collect(Collectors.toList());
+	}
+	public List<NhanVien> getDSLonNhat(){
+		double maxDoanhSo = quanLy.stream()
+				.filter(nv -> nv instanceof NhanVienBanHang)
+				.mapToDouble(nv -> ((NhanVienBanHang)nv).getDoanhSo())
+				.max()
+				.orElse(0);
+		return quanLy.stream()
+				.filter(nv -> nv instanceof NhanVienBanHang nvbh
+						&& nvbh.getDoanhSo() == maxDoanhSo)
+				.collect(Collectors.toList());
+	}
+	public boolean capNhatNhanVien(String maSo, double heSoLuongMoi) {
+		for (NhanVien nhanVien : quanLy) {
+			if(nhanVien instanceof NhanVienVanPhong nvvp && nvvp.getMaSo().equalsIgnoreCase(maSo)) {
+				nvvp.setHeSoLuong(heSoLuongMoi);
+				return true;
+			}
+		}
+		return false;
+	}
+}
